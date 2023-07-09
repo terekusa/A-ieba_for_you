888f2a8c3ace94c6e029ca06adf73225b5a92001
Taku Fukaura <terekusaitama@gmail.com>
2023-07-09 18:35:31 +0900

    [add] Likeのajax化 #8

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
     redirect_to posts_path, success: t('.success')
   end
 
   def destroy
-    post = current_user.likes.find(params[:id]).post
-    current_user.unlike(post)
+    @post = current_user.likes.find(params[:id]).post
+    current_user.unlike(@post)
     redirect_to posts_path, success: t('.success')
   end
 end
