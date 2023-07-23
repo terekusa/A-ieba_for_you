40e7ee0de8cc8f7c5377bcc406efe944ecd7e0f4
Taku Fukaura <terekusaitama@gmail.com>
2023-07-23 21:54:27 +0900

    [add] ゲストログイン機能の追加 #22

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  post '/guest_login', to: 'user_sessions#guest_login'
 
   resources :users, only: %i[new create show]
   resources :posts do
