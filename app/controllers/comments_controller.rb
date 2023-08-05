f557107a80978b36e68a904b93159c651d9ec866
Taku Fukaura <terekusaitama@gmail.com>
2023-08-05 15:55:08 +0900

    [fix] コメントコントローラの修正 #4

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,13 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.save
+    redirect_to request.referer
   end
 
   def destroy
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
+    redirect_to request.referer
   end
 
   private
