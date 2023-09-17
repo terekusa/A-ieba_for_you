26c65f91baa0e106a43d823329e0959eb3e62c61
Taku Fukaura <terekusaitama@gmail.com>
2023-09-17 21:07:40 +0900

    #93_いいねボタンを押した際の挙動を修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,18 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
-    redirect_to request.referer
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.turbo_stream
+    end
   end
 
   def destroy
     @post = current_user.likes.find(params[:id]).post
     current_user.unlike(@post)
-    redirect_to request.referer
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.turbo_stream
+    end
   end
 end
